b604cf4d78ddf3087257b67659af10483b173275
xiety <xiety666@gmail.com>
2024-02-07 14:18:19 +0200

    map

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let filename = r"d:\dev\Projects_Rust\advent_app_data\2024_01_A_input.txt";
+    let filename = r"..\advent_app_data\2024_01_A_input.txt";
     let lines = read_lines(filename);
     let result = lines.into_iter().map(|x| find_digit_in_string(&x)).sum::<u32>();
 
@@ -17,21 +17,7 @@ fn read_lines(filename: &str) -> Vec<String> {
 }
 
 fn find_digit_in_string(string: &str) -> u32 {
-    let mut dig = 0;
-
-    for c in string.chars() {
-        if c.is_digit(10) {
-            dig = 10 * c.to_digit(10).unwrap();
-            break;
-        }
-    }
-
-    for c in string.chars().rev() {
-        if c.is_digit(10) {
-            dig += c.to_digit(10).unwrap();
-            break;
-        }
-    }
-
-    dig
+    let val1 = string.chars().filter(|c| c.is_digit(10)).next().unwrap().to_digit(10).unwrap();
+    let val2 = string.chars().rev().filter(|c| c.is_digit(10)).next().unwrap().to_digit(10).unwrap();
+    val1 * 10 + val2
 }
