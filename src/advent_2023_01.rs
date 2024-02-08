9397dbaef8cd46c5585cfe882f627a2009dec16e
xiety <xiety666@gmail.com>
2024-02-08 14:14:40 +0200

    unit tests

---
+++ src/advent_2023_01.rs
@@ -0,0 +1,52 @@
+use std::fs::read_to_string;
+
+#[allow(dead_code)]
+fn run(filename: &str) -> u32 {
+    read_lines(filename)
+        .into_iter()
+        .map(|x| find_digit_in_string(&x))
+        .sum::<u32>()
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+fn find_digit_in_string(string: &str) -> u32 {
+    let val1 = get_digit(string.chars());
+    let val2 = get_digit(string.chars().rev());
+    val1 * 10 + val2
+}
+
+fn get_digit<T>(chars: T) -> u32
+where
+    T: Iterator<Item = char>,
+{
+    chars
+        .filter(|c| c.is_digit(10))
+        .next()
+        .unwrap()
+        .to_digit(10)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_2023_01_a_sample() {
+        let actual = run(r"..\advent_app_data\2024_01_A_sample.txt");
+        assert_eq!(actual, 142);
+    }
+
+    #[test]
+    fn test_2023_01_a_input() {
+        let actual = run(r"..\advent_app_data\2024_01_A_input.txt");
+        assert_eq!(actual, 54644);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
-use std::fs::read_to_string;
+mod advent_2023_01;
 
 fn main() {
-    let filename = r"..\advent_app_data\2024_01_A_input.txt";
-    let lines = read_lines(filename);
-    let result = lines
-        .into_iter()
-        .map(|x| find_digit_in_string(&x))
-        .sum::<u32>();
-
-    println!("{result}")
-}
-
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
-}
-
-fn find_digit_in_string(string: &str) -> u32 {
-    let val1 = get_digit(string.chars());
-    let val2 = get_digit(string.chars().rev());
-    val1 * 10 + val2
-}
-
-fn get_digit<T>(chars: T) -> u32
-where
-    T: Iterator<Item = char>,
-{
-    chars
-        .filter(|c| c.is_digit(10))
-        .next()
-        .unwrap()
-        .to_digit(10)
-        .unwrap()
+    println!("Run 'cargo test'")
 }
