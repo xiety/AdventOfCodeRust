0b7716ac70b6b905581bcc4a1ff2aa431c7b1844
xiety <xiety666@gmail.com>
2024-02-08 13:27:22 +0200

    IntoIterator

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::fs::read_to_string;
 fn main() {
     let filename = r"..\advent_app_data\2024_01_A_input.txt";
     let lines = read_lines(filename);
-    let result = lines.into_iter().map(|x| find_digit_in_string(&x)).sum::<u32>();
+    let result = lines
+        .into_iter()
+        .map(|x| find_digit_in_string(&x))
+        .sum::<u32>();
 
     println!("{}", result)
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename) 
+    read_to_string(filename)
         .unwrap()
         .lines()
         .map(String::from)
@@ -17,7 +20,20 @@ fn read_lines(filename: &str) -> Vec<String> {
 }
 
 fn find_digit_in_string(string: &str) -> u32 {
-    let val1 = string.chars().filter(|c| c.is_digit(10)).next().unwrap().to_digit(10).unwrap();
-    let val2 = string.chars().rev().filter(|c| c.is_digit(10)).next().unwrap().to_digit(10).unwrap();
+    let val1 = get_digit(string.chars());
+    let val2 = get_digit(string.chars().rev());
     val1 * 10 + val2
+}
+
+fn get_digit<T>(chars: T) -> u32
+where
+    T: IntoIterator<Item = char>,
+{
+    chars
+        .into_iter()
+        .filter(|c| c.is_digit(10))
+        .next()
+        .unwrap()
+        .to_digit(10)
+        .unwrap()
 }
