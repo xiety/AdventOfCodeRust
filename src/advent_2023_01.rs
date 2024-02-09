9afdb426cd56461bfdddc04334fe55b8c5f3c735
xiety <xiety666@gmail.com>
2024-02-09 23:12:03 +0200

    b works

--- src/advent_2023_01.rs
+++ src/advent_2023_01.rs
@@ -1,15 +1,18 @@
+use std::collections::HashMap;
+
 use crate::helpers;
 use crate::helpers::IteratorExt;
 
 #[allow(dead_code)]
-fn run(filename: &str) -> u32 {
+
+fn run_a(filename: &str) -> u32 {
     helpers::read_lines(filename)
         .into_iter()
-        .map(|x| find_digit_in_string(&x))
+        .map(|x| process_a(&x))
         .sum::<u32>()
 }
 
-fn find_digit_in_string(string: &str) -> u32 {
+fn process_a(string: &str) -> u32 {
     let val1 = get_digit(string.chars());
     let val2 = get_digit(string.chars().rev());
     val1 * 10 + val2
@@ -26,22 +29,96 @@ where
         .unwrap()
 }
 
+fn run_b(filename: &str) -> u32 {
+    helpers::read_lines(filename)
+        .into_iter()
+        .map(|x| process_b(&x))
+        .sum::<u32>()
+}
+
+fn process_b(line: &str) -> u32 {
+    let dic = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+
+    let mut min = Option::None;
+    let mut min_result = String::from("");
+
+    let mut max = Option::None;
+    let mut max_result = String::from("");
+
+    for (key, value) in &dic {
+
+        let on = line.find(key);
+
+        if min == Option::None || (on.is_some() && on.unwrap() < min.unwrap()) {
+            min = on;
+            min_result = value.to_string();
+        }
+
+        let onl = line.rfind(key);
+
+        if max == Option::None || (onl.is_some() && onl.unwrap() > max.unwrap()) {
+            max = onl;
+            max_result = value.to_string();
+        };
+    }
+
+    let join = format!("{min_result}{max_result}");
+    let result = join.parse().unwrap();
+
+    println!("{line} {result}");
+
+    result
+}
+
 #[cfg(test)]
 mod test {
     #![allow(non_snake_case)]
 
-    use super::run;
+    use super::run_a;
+    use super::run_b;
     use crate::test_base::get_test_file_name;
 
     #[test]
     fn test_2023_01_A_sample() {
-        let actual = run(&get_test_file_name(2023, 1, "A", false));
+        let actual = run_a(&get_test_file_name(2023, 1, "sampleA"));
         assert_eq!(actual, 142);
     }
 
     #[test]
     fn test_2023_01_A_input() {
-        let actual = run(&get_test_file_name(2023, 1, "A", true));
+        let actual = run_a(&get_test_file_name(2023, 1, "input"));
         assert_eq!(actual, 54644);
+    }
+
+    #[test]
+    fn test_2023_01_B_sample() {
+        let actual = run_b(&get_test_file_name(2023, 1, "sampleB"));
+        assert_eq!(actual, 281);
+    }
+
+    #[test]
+    fn test_2023_01_B_input() {
+        let actual = run_b(&get_test_file_name(2023, 1, "input"));
+        assert_eq!(actual, 53348);
     }
 }
