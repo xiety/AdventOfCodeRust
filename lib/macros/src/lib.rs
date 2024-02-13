fd0d6e5741a0f5e67552b229b0d53407ca13cf97
xiety <xiety666@gmail.com>
2024-02-13 20:08:58 +0200

    macros

--- lib/macros/src/lib.rs
+++ lib/macros/src/lib.rs
@@ -1,39 +1,16 @@
 use proc_macro::TokenStream;
 
 use quote::{format_ident, quote};
-use regex_lite::Regex;
 use syn::parse_macro_input;
 use syn::ItemStruct;
-
-#[proc_macro_attribute]
-pub fn macro_regex_test(_attr: TokenStream, _item: TokenStream) -> TokenStream {
-    let fs = quote! {
-        //hello
-    };
-
-    let ts = TokenStream::from(fs);
-
-    println!("{}", ts);
-
-    ts
-}
 
 #[proc_macro_attribute]
 pub fn macro_regex(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
     let attr = parse_macro_input!(attr as syn::LitStr);
-    let reg = Regex::new(&attr.value()).expect("Invalid regex");
 
     let mut parsers = Vec::new();
     let mut fields = Vec::new();
-
-    if input.fields.len() == 0 {
-        panic!("Struct must have at least one field");
-    }
-
-    if input.fields.len() != reg.captures_len() - 1 {
-        panic!("Number of regex captures must equal number of struct fields")
-    }
 
     for (i, f) in input.fields.iter().enumerate() {
         let v = format_ident!("__field_{}", i);
@@ -41,7 +18,7 @@ pub fn macro_regex(attr: TokenStream, item: TokenStream) -> TokenStream {
         let id = &f.ident;
 
         parsers.push(quote! {
-            let #v: #ty = caps.get(#i+1).ok_or("Missing capture")?.as_str().parse()?;
+            let #v: #ty = caps.get(#i+1).unwrap().as_str().parse().unwrap();
         });
 
         fields.push(quote! {
@@ -50,11 +27,6 @@ pub fn macro_regex(attr: TokenStream, item: TokenStream) -> TokenStream {
     }
 
     let id = &input.ident;
-    let reg = format_ident!("__REGEX_{}", id.to_string().to_uppercase());
-
-    let instance = quote! { Self {
-        #(#fields)*
-    } };
 
     let fs = quote! {
 
@@ -62,11 +34,16 @@ pub fn macro_regex(attr: TokenStream, item: TokenStream) -> TokenStream {
 
         impl ::std::str::FromStr for #id {
             type Err = Box<dyn ::std::error::Error>;
+
             fn from_str(s: &str) -> Result<Self, Self::Err> {
-                let #reg: ::regex_lite::Regex = ::regex_lite::Regex::new(#attr).unwrap();
-                let caps = #reg.captures(s).ok_or("No match")?;
+                let reg: ::regex_lite::Regex = ::regex_lite::Regex::new(#attr).unwrap();
+                let caps = reg.captures(s).unwrap();
+
                 #(#parsers)*
-                Ok(#instance)
+
+                Ok(Self {
+                    #(#fields)*
+                })
             }
         }
     };
